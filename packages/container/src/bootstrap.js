579c139e2ff815f82bf9c5b1a52f233e8a71acb7
Caleb Hernández <101578626+neo-zero98-2@users.noreply.github.com>
2024-07-28 11:52:32 -0600

    prueba deployd

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(
-        <App/>,
+ReactDOM.render(    
+    <React.Fragment>
+        <App/>
+    </React.Fragment>,
     document.querySelector('#root')
 );
