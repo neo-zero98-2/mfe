d6c211a9a3669d8cc9c00cf21ded40add268ca60
Caleb Hernandez <hern98_500@hotmail.com>
2024-07-28 23:39:16 -0600

    fix: resolve coalision css between container and marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,27 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+/**
+ * evita conflictos cuando se compila para prod
+ * genera prefijos de clases que empiezan con co y esto evita que 
+ * se haga una coalición de clases entre distintos microfronts con 
+ * la misma libreria css in js como material ui
+ */
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
 
 export const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <MarketingApp />
-      </BrowserRouter>
-    </div>
-
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import Pricing from './components/Pricing';
 import Album from './components/Landing';
+
+/**
+ * evita conflictos cuando se compila para prod
+ * genera prefijos de clases que empiezan con ma y esto evita que 
+ * se haga una coalición de clases entre distintos microfronts con 
+ * la misma libreria css in js como material ui
+ */
+const generateClassName = createGenerateClassName({ 
+    productionPrefix: 'ma' 
+});
 
 export default () => {
     return (
         <React.Fragment>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path='/pricing' component={ Pricing }/>
